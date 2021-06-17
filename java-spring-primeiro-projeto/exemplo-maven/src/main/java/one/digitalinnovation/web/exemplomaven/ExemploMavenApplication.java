4aff350d6f6c67538442da387ee949a61436551c
edvaldoljr <edvaldoluizj@gmail.com>
2021-06-17 09:21:54 -0300

    Criando Projeto com Gradle e Maven com Spring

---
+++ java-spring-primeiro-projeto/exemplo-maven/src/main/java/one/digitalinnovation/web/exemplomaven/ExemploMavenApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.web.exemplomaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploMavenApplication.class, args);
+	}
+
+}
---
+++ java-spring-primeiro-projeto/exemplo-maven/src/test/java/one/digitalinnovation/web/exemplomaven/ExemploMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.web.exemplomaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
